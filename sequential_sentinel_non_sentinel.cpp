38cb1a212cbc8eefc13734cc16f7827b21e0b6fa
YudGir <yudhagirsang07@gmail.com>
2026-03-27 20:49:59 +0700

    mengupdate kodingan utk sentinel searching

--- sequential_sentinel_non_sentinel.cpp
+++ sequential_sentinel_non_sentinel.cpp
@@ -1,25 +1,55 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main () {
+//     int data[] = {10, 15, 35, 40, 60};
+//     int banyakElemenArrayData = 5;
+//     int angkaUserYangDicari;
+//     int indeks = -1;
+
+//     cout << "Hi!\n";
+//     cout << "Mau cari angka berapa dari array? ";
+//     cin >> angkaUserYangDicari;
+
+//     for (int i = 0; i < banyakElemenArrayData; i++) {
+//         if (data[i] == angkaUserYangDicari) {
+//             indeks = i;
+//         }
+//     }
+
+//     if (indeks != -1) cout << "Yeay, ketemu.\n";
+//     else cout << "Yah, ga ketemu brok!\n";
+    
+//     ------------------------------- | THIS IS SEQUENTIAL SEARCHING: NON-SENTINEL 
+// }
+
 #include <iostream>
 
 using namespace std;
 
 int main () {
-    int data[] = {10, 15, 35, 40, 60};
-    int banyakElemenArrayData = 5;
-    int angkaUserYangDicari;
-    int indeks = -1;
+    int data[] = {1,2,3,4,5};
+    int JumlahElemen = 5, i = 0;
+    int angkaUserYangDicari; 
 
     cout << "Hi!\n";
     cout << "Mau cari angka berapa dari array? ";
     cin >> angkaUserYangDicari;
 
-    for (int i = 0; i < banyakElemenArrayData; i++) {
-        if (data[i] == angkaUserYangDicari) {
-            indeks = i;
-        }
+    int angkaTerakhir = data[JumlahElemen - 1]; //inisialisasi elemen akhir array = posisi array di paling ujung
+    data[JumlahElemen - 1] = angkaUserYangDicari; //inisialisasi nilai di ujung array adalah angka yg user cari
+
+    while (data[i] != angkaUserYangDicari) {
+        i++; //terus ulangi while-iteration sampai data[i] = angka user yg diinginkan utk dicari
     }
 
-    if (indeks != -1) cout << "Yeay, ketemu.\n";
-    else cout << "Yah, ga ketemu brok!\n";
-    
-    //THIS IS SEQUENTIAL NON-SENTINEL SEARCHING
+    data[JumlahElemen - 1] = angkaTerakhir; //buat ngembaliin nilai array paling terakhir menjadi semestinya
+
+    if (i < JumlahElemen - 1)   
+        cout << "mantap, ketemu brok di indeks ke- " << i << ". (AI, Asli Itu) HAHA\n";
+    else if (data[JumlahElemen - 1] == angkaUserYangDicari)
+        cout << "ketemu sih, tapi si Sentinel-nya bro!\n";
+    else 
+        cout << "yah, ga ketemu brok!\n";
 }
